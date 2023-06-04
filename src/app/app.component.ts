fa03c0c8c0d165ef0094c1bcdb8d1e3b2bbf9d39
IlliaShein <illiasheinillia@gmail.com>
2023-06-04 09:19:05 +0300

    Added output of github account information, header now has 100% width

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'GitHubFind';
+
+  userName : string = "";
+  response : any;
+
+  constructor(private http: HttpClient){
+
+  }
+
+  Search(){
+    this.http.get('https://api.github.com/users/' + this.userName)
+    .subscribe((response) => {
+      this.response = response;
+      console.log(this.response);
+    });
+  }
+
+  getObjectKeys(obj: any): string[] {
+    return Object.keys(obj);
+  }
 }
